56ac929244f26cd184420778a36b44e90d6a308a
senselogic <dev.senselogic@gmail.com>
2023-09-05 06:09:24 +0200

    % mixin

--- TEST/src/routes/+page.svelte
+++ TEST/src/routes/+page.svelte
@@ -4,6 +4,6 @@
 <h1 class="font-size-200 font-weight-700 color-red">
     Welcome to SvelteKit
 </h1>
-<p class="margin-top-100 color-green-500">
+<p class="display-flex gap-100 margin-top-100 color-green-500">
     Visit <a class="color-green-300" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>

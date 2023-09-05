c3105528230d0dab9479578cb21dc12ea06fc274
senselogic <dev.senselogic@gmail.com>
2023-09-05 05:48:04 +0200

    + mixin

--- TEST/src/routes/+page.svelte
+++ TEST/src/routes/+page.svelte
@@ -1,13 +1,9 @@
 <style lang="stylus">
-    @import 'senselogic-flow/src/constant.styl';
-    @import 'senselogic-flow/src/color.styl';
-
-    h1
-    {
-        color: redColor500;
-        font-size: 2rem;
-    }
 </style>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a class="color-green" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="font-size-200 font-weight-700 color-red">
+    Welcome to SvelteKit
+</h1>
+<p class="margin-top-100 color-green-500">
+    Visit <a class="color-green-300" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>

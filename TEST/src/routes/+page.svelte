ee228eb2169cc11efa0e53b308121895c9a8213a
senselogic <dev.senselogic@gmail.com>
2023-09-07 21:56:48 +0200

    % style

--- TEST/src/routes/+page.svelte
+++ TEST/src/routes/+page.svelte
@@ -7,3 +7,28 @@
 <p class="display-flex gap-100 margin-top-100 color-green-500">
     Visit <a class="color-green-300" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>
+
+<label>
+    <span class="switch-input">
+         <input class="switch-input-checkbox" type="checkbox"/>
+         <span class="switch-input-slider">
+    </span>
+    <span>Switch</span>
+</label>
+
+<input class="input" type="text"/>
+
+<textarea class="text-input">
+</textarea>
+
+<select class="enumeration-input">
+    <option class="enumeration-input-option">
+        First option
+    </option>
+    <option class="enumeration-input-option">
+        Second option
+    </option>
+    <option class="enumeration-input-option">
+        Third option
+    </option>
+</select>

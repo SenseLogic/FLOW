07586bdc0e321c0f836d55cadfcd022718297a1d
senselogic <dev.senselogic@gmail.com>
2023-09-09 09:39:28 +0200

    % style

--- TEST/src/routes/+page.svelte
+++ TEST/src/routes/+page.svelte
@@ -1,34 +1,39 @@
 <style lang="stylus">
 </style>
 
-<h1 class="font-size-200 font-weight-700 color-red">
-    Welcome to SvelteKit
-</h1>
-<p class="display-flex gap-100 margin-top-100 color-green-500">
-    Visit <a class="color-green-300" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<div class="padding-100 padding-lateral-200-large">
+    <h1 class="font-size-200 font-weight-700 color-red">
+        Welcome to SvelteKit
+    </h1>
+    <p class="margin-top-100 color-green-500">
+        Visit <a class="color-green-300 color-green-500-hover" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+    </p>
 
-<label>
-    <span class="switch-input">
-         <input class="switch-input-checkbox" type="checkbox"/>
-         <span class="switch-input-slider">
-    </span>
-    <span>Switch</span>
-</label>
+    <div class="margin-top-100 display-flex flex-direction-column gap-100">
+        <div class="display-flex gap-50">
+            <span class="switch-input-container">
+                <input class="switch-input" type="checkbox"/>
+                <span class="switch-input-slider">
+                </span>
+            </span>
+            <span>Switch</span>
+        </div>
 
-<input class="input" type="text"/>
+        <input class="input" type="text"/>
 
-<textarea class="text-input">
-</textarea>
+        <textarea class="text-input">
+        </textarea>
 
-<select class="enumeration-input">
-    <option class="enumeration-input-option">
-        First option
-    </option>
-    <option class="enumeration-input-option">
-        Second option
-    </option>
-    <option class="enumeration-input-option">
-        Third option
-    </option>
-</select>
+        <select class="enumeration-input">
+            <option class="enumeration-input-option">
+                First option
+            </option>
+            <option class="enumeration-input-option">
+                Second option
+            </option>
+            <option class="enumeration-input-option">
+                Third option
+            </option>
+        </select>
+    </div>
+</div>

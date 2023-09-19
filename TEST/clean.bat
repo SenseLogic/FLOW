97f194fb792695948f0c10062c1680863ce74329
senselogic <dev.senselogic@gmail.com>
2023-09-19 19:34:43 +0200

    % stylus

---
+++ TEST/clean.bat
@@ -0,0 +1,2 @@
+rmdir /s /q node_modules
+pause
--- TEST/svelte.config.js
+++ TEST/svelte.config.js
@@ -1,14 +1,10 @@
 import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import sveltePreprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    preprocess: vitePreprocess({
+    preprocess: sveltePreprocess({
       stylus: {
-        stylus: {
-          // options for stylus compiler
-        },
-        // options for stylus plugin
       },
     }),
 
